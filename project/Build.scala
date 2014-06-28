1df4fbf6b59a2f1ff900fcd1c519d80519b810a4
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2014-06-28 19:13:15 +0600

    Added travis config file

--- project/Build.scala
+++ project/Build.scala
@@ -32,17 +32,21 @@ object ApplicationBuild extends Build
     // make sure that MultiJvm test are compiled by the default test compilation
     compile in MultiJvm <<= (compile in MultiJvm) triggeredBy (compile in Test),
     // disable parallel tests
-    parallelExecution in Test := false
-//    // make sure that MultiJvm tests are executed by the default test target
-//    executeTests in Test <<=
-//      ((executeTests in Test), (executeTests in MultiJvm)) map {
-//		  case (testResults, multiJvmResults) =>
-//		  	Tests.Output(
-//		  		Tests.overall(List(testResults.overall, multiJvmResults.overall)),
-//				testResults.events ++ multiJvmResults.events,
-//				testResults.summaries ++ multiJvmResults.summaries)
-//      }
-    )
+    parallelExecution in Test := false,
+    // make sure that MultiJvm tests are executed by the default test target,
+    // and combine the results from ordinary test and multi-jvm tests
+    executeTests in Test <<= (executeTests in Test, executeTests in MultiJvm) map {
+      case (testResults, multiNodeResults)  =>
+        val overall =
+          if (testResults.overall.id < multiNodeResults.overall.id)
+            multiNodeResults.overall
+          else
+            testResults.overall
+        Tests.Output(overall,
+          testResults.events ++ multiNodeResults.events,
+          testResults.summaries ++ multiNodeResults.summaries)
+    }
+  )
 
 	val buildSettings = Defaults.defaultSettings ++ multiJvmSettings ++
     Seq (
